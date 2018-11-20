5c4c720b522f471216f550edd8d130094ed98f3c
Alex <aberman12@yahoo.com>
2018-11-19 23:34:48 -0800

    connected music api

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList";
+
+// type Props = {};
+//<Props>
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerText={"Albums!"} />
+        <AlbumList />
+      </View>
+    );
+  }
+}
